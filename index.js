cb4411ad880c403bbac83a674a7361802ac7316c
Jonathan <jonathan.fetterolf@gmail.com>
2023-04-27 15:15:26 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Follow along with the examples here
+function doNothin() {}
+
+function sayHello() {
+    console.log("Hello!");
+}
+
+sayHello();
+
+function sayHelloToGuadalupe() {
+console.log("Hello, Guadalupe!");
+}
+
+function sayHelloToLiz() {
+console.log("Hello, Liz!");
+}
+
+function sayHelloToSamip() {
+console.log("Hello, Samip!");
+}
+
+function sayHelloTo(firstName) {
+console.log(`Hello, ${firstName}!`);
+}
+
+function say(greeting, firstName) {
+return `${greeting}, ${firstName}!`;
+}
+
+
+function add(x, y) {
+    return x + y;
+  }
+  
+console.log(add(67, 2));
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+sayHelloTo(4);
+console.log(say("Hello", "monkey"));
